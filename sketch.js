fe6fa11cd45f51b2d9212bbf4d14518fcb30e0d2
CrazySongbird13 <84483835+CrazySongbird13@users.noreply.github.com>
2021-07-28 20:55:29 -0400

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,35 +1,69 @@
-var car, wall;
-var speed, weight;
+var wall, thickness;
+var bullet,speed, weight;
 
 function setup() {
-  createCanvas(1600,400);
-  car = createSprite(50, 200, 50, 50);
-  wall = createSprite(1500, 200, 60, height/2);
-  speed = random(55, 90);
-  weight = random(400, 1500)
+  createCanvas(1600, 400);
+
+  speed=random(223,321)
+  weight=random(32,50)
+
+
+  thickness=random(25,80)
+
+
+
+
+  
+    bullet=createSprite(50, 200, 20,10);  
+    bullet.velocityX = speed;
+    bullet.shapeColor=color(255);
+
+ 
+  	
+    
+    wall=createSprite(1200, 200, thickness, height/2);  
+
+    wall.shapeColor=color(220,220, 220);
 }
 
+
 function draw() {
-  background(255,255,255);  
-  if(wall.x-car.x < (car.width+wall.width)/2)
+  background(0);  
+  //bullet.sprite.collide(wall.sprite,calculateDeformation)
+  if(hasCollided(bullet, wall))
   {
-    car.velocityX = 0;
-    var deformation = 0.5 * weight * speed* speed/22500;
-     if(deformation>180)
-    {
-      car.shapeColor = color(255, 0, 0);
-    }
+  	bullet.velocityX=0;
+  	var damage=0.5 * weight * speed* speed/(thickness *thickness *thickness);
 
-    if(deformation < 180 && deformation > 100);
-    {
-     car.shapeColor = color(230, 230, 0);
-    }
-     IDBCursor(deformation < 100)
-     {
-       car.shapeColor = color(0, 255, 0);
-     }
+  	
+	if(damage>10)
+	{
+		wall.shapeColor=color(255,0,0);
+		
+	}
 
+	
+
+	if(damage<10)
+	{
+		wall.shapeColor=color(0,255,0);
+	}
+	
   }
-  car.velocityX = speed;
+
+
   drawSprites();
+ 
+}
+
+
+function hasCollided(lbullet, lwall)
+{
+	bulletRightEdge=lbullet.x +lbullet.width;
+	wallLeftEdge=lwall.x;
+	if (bulletRightEdge>=wallLeftEdge)
+	{
+		return true
+	}
+	return false;
 }
